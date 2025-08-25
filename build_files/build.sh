32f89be6d0bc227bfe1ebe96203530569b26a619
kawa-sanmyaku <shiera.wa.kaimononiiku@gmail.com>
2025-08-24 22:28:07 -0600

    that didn't work, must be done another way

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,6 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y inkscape kitty krita neovim
 dnf5 remove -y konsole kwrite vim
-flatpak install --system -y com.heroicgameslauncher.hgl
 
 # Use a COPR Example:
 #
